54b90d5213100b29e6c77d39356564aae96dc29e
Konstantin Polyakov <shyzik_93@mail.ru>
2018-08-11 18:02:31 +0300

    file for building full CMS was added

---
+++ builder.sh
@@ -0,0 +1,23 @@
+git submodule init
+git submodule update
+
+cd modules/wbs_core
+git submodule init
+git submodule update
+cd ../../
+
+# load engine
+
+wget -O engine.zip http://addon.websitebaker.org/pages/en/browse-add-ons.php?download=0EC5EAC3
+unzip engine.zip
+rm engine.zip
+mv ./config.php.new ./config.php
+
+# creating the archive
+
+#name=`date +%Y%m%d-%H%M%S`
+#tar -cz ./* > ../../engine-$name.tar.gz
+#rm -rv ./*
+
+# Done :)
+echo "done :-)"
